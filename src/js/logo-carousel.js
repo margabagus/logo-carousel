c58e366b01ce4e800df2fee4a92600c09cd4e3d6
Marga Bagus <margabagus@gmail.com>
2024-12-21 14:58:43 +0700

    Update logo-carousel.js
    Fixing file format

--- src/js/logo-carousel.js
+++ src/js/logo-carousel.js
@@ -1,1 +1,93 @@
-!function(t){"use strict";var n=function(n,e){this.$element=t(n),this.$container=this.$element.find(".logo-container"),this.$slides=this.$element.find(".logo-slide"),this.options=t.extend({},{autoplay:!0,speed:100,pauseOnHover:!0},e),this.init()};n.prototype.init=function(){this.$slides.clone().appendTo(this.$container),this.options.pauseOnHover&&this.initHoverEvents(),this.initResponsive()},n.prototype.initHoverEvents=function(){var t=this;this.$element.hover((function(){t.$container.css("animation-play-state","paused")}),(function(){t.$container.css("animation-play-state","running")}))},n.prototype.initResponsive=function(){t(window).on("resize",(function(){}))},t.fn.logoCarousel=function(e){return this.each((function(){var i=t(this);i.data("logoCarousel")||i.data("logoCarousel",new n(this,e))}))},t(document).ready((function(){t(".logo-carousel").each((function(){var n=t(this),e={autoplay:!1!==n.data("autoplay"),speed:n.data("speed")||100,pauseOnHover:!1!==n.data("pause-hover")};t(this).logoCarousel(e)}))}))}(jQuery);
+/**
+ * Logo Carousel
+ * Version: 1.0.0
+ * Author: Marga Bagus
+ * License: MIT
+ * https://github.com/margabagus/logo-carousel
+ */
+
+(function($) {
+    'use strict';
+
+    // Logo Carousel Class
+    var LogoCarousel = function(element, options) {
+        this.$element = $(element);
+        this.$container = this.$element.find('.logo-container');
+        this.$slides = this.$element.find('.logo-slide');
+        
+        // Default options
+        this.options = $.extend({}, {
+            autoplay: true,
+            speed: 100,
+            pauseOnHover: true
+        }, options);
+
+        this.init();
+    };
+
+    // Initialize plugin
+    LogoCarousel.prototype.init = function() {
+        // Clone slides for continuous effect
+        this.$slides.clone().appendTo(this.$container);
+
+        // Initialize hover events if enabled
+        if (this.options.pauseOnHover) {
+            this.initHoverEvents();
+        }
+
+        // Initialize responsive behavior
+        this.initResponsive();
+    };
+
+    // Handle hover events
+    LogoCarousel.prototype.initHoverEvents = function() {
+        var self = this;
+        
+        this.$element.hover(
+            function() {
+                self.$container.css('animation-play-state', 'paused');
+            },
+            function() {
+                self.$container.css('animation-play-state', 'running');
+            }
+        );
+    };
+
+    // Handle responsive behavior
+    LogoCarousel.prototype.initResponsive = function() {
+        var self = this;
+        
+        $(window).on('resize', function() {
+            // Add responsive logic if needed
+        });
+    };
+
+    // jQuery plugin definition
+    $.fn.logoCarousel = function(options) {
+        return this.each(function() {
+            var $this = $(this);
+            var data = $this.data('logoCarousel');
+            
+            if (!data) {
+                $this.data('logoCarousel', 
+                    new LogoCarousel(this, options)
+                );
+            }
+        });
+    };
+
+    // Auto-initialize on DOM ready
+    $(document).ready(function() {
+        $('.logo-carousel').each(function() {
+            var $carousel = $(this);
+            var options = {
+                autoplay: $carousel.data('autoplay') !== false,
+                speed: $carousel.data('speed') || 100,
+                pauseOnHover: $carousel.data('pause-hover') !== false
+            };
+            
+            $(this).logoCarousel(options);
+        });
+    });
+
+})(jQuery);
